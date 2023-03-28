85777fe1a3951aae9f7c27b02fe316f1f70e2c25
danicaclarize <100770381+danicaclarize@users.noreply.github.com>
2023-03-28 22:32:35 +0800

    edited the  comments

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// 1. Find the element
+
 var square = document.getElementById('square');
 var words = document.getElementById('words');
 
 
-// 2. Create Fuctionality
+// 2. Create functionality
 function greeting() {
         words.innerHTML = "Welcome to Flatland <br> I am square";
 }
@@ -13,7 +15,6 @@ function clicked() {
     //var msg = "Build a <br>" + createBuzzwordPhrase();
     //words.innerHTML = msg;
     words.innerHTML = "Build a <br>" + createBuzzwordPhrase();
-
 }
 // Call greeting function when document is loaded
 document.onload = greeting()
@@ -27,14 +28,15 @@ function createBuzzwordPhrase() {
     let idx_buz = Math.floor(Math.random() * buzz.length);
     let idx_act = Math.floor(Math.random() * action.length);
     let idx_out = Math.floor(Math.random() * outcome.length);
-
     return  buzz[idx_buz] + " " + action[idx_act] + " " + outcome[idx_out];
 }
-// 3. Add an event listener
+
 // Test the createBuzzwordPhrase function by calling it in the console
 console.log(createBuzzwordPhrase()); // Use () to call the function
 
+// 3. Add an event listener
 
+/* Create a click event*/
 square.addEventListener('click', (event) => clicked());
 
 /* create hover effect*/
